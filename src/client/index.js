76472f543b956d8c9d8ad97507adbd6da87da0bb
Justin Knutson <jknutson@alumni.princeton.edu>
2021-03-19 14:47:52 -0600

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -28,7 +28,7 @@ ReactDOM.render(
               Triumvirate Countdown
             </h1>
             <Typography variant="subheading">
-              To Be Determined...
+              <Countdown updateInterval={100} endTime={new Date('April 24, 2021 12:00:00')} />
             </Typography>
             <p>
               <em>
