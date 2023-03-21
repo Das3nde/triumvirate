47dd3a642c25dc98c8a5e626ccae8b8800debe77
Das3nde <knutson.justin@gmail.com>
2023-03-21 15:37:40 -0400

    Changed Port use to be dynamic

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
-app.use(express.static('dist/triumvirate'));
+app.use(express.static(kdist/triumvirate'));
 
 app.get('/', function(req, res) {
     res.sendFile(path.resolve('dist/triumvirate/index.html'));
